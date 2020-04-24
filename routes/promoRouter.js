de80538ff67ad00a35e035be58d6afe94d15321d
Sathyajith95 <sathyajith95@gmail.com>
2020-04-24 16:38:32 +0530

    Express Generator REST API

---
+++ routes/promoRouter.js
@@ -0,0 +1,53 @@
+const express = require('express');
+const bodyParser = require('body-parser');
+
+const promoRouter = express.Router();
+
+promoRouter.use(bodyParser.json());
+
+promoRouter.route('/')
+.all((req,res,next)=> {
+    res.statusCode = 200;
+    res.setHeader('Content-Type', 'text/plain');
+    next();
+})
+.get((req,res,next)=>{
+    res.end('Will send all the promotions to you!');
+})
+.post((req,res,next)=> {
+    res.end('Will add the promotion: '+ req.body.name + 
+    ' with details: '+ req.body.description);
+})
+.put((req,res,next)=> {
+    res.statusCode = 403;
+    res.end('PUT operation not suported on /promotions')
+})
+.delete((req,res,next)=> {
+    res.end('Deleting all the promotions!');
+});
+
+//for /promotions/:promotionId
+
+promoRouter.route('/:promotionId')
+.get((req,res,next)=>{
+    res.end('Will send details of the promotion: '
+        + req.params.promotionId + ' to you!');
+})
+
+.post((req,res,next)=> {
+    res.statusCode = 403;
+    res.end('POST operation not suported on /promotions/'
+        + req.params.promotionId );
+})
+
+.put((req,res,next)=> {
+    res.write('Updating the promotion: '+ req.params.promotionId);
+    res.end('Will update the promotion: '+ req.body.name + 
+    ' with details: ' + req.body.description);
+})
+
+.delete((req,res,next)=> {
+    res.end('Deleting promotion: '+ req.params.promotionId);
+});
+
+module.exports = promoRouter;
